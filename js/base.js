ba8ccc8b700fb8f1c3c099045266da001a69a70d
halabe15 <halabe15@gmail.com>
2018-11-01 10:43:04 -0600

    avances

--- js/base.js
+++ js/base.js
@@ -9,19 +9,80 @@ camera = null,
 root = null,
 player = null,
 car = null,
+log = null,
 group = null,
 orbitControls = null;
 
 var animated = false;
 
+var trees = null,
+cars = null,
+logs = null;
+
+var pathsPositions = [];
+
+var roadsPosition = [];
+var riversPosition = [];
+var grassPosition = [];
+
 var playerBBox = null;
-var treeBBox = null;
-var carBBox = null;
+var treeBBox = [];
+var carBBox = [];
+var logBBox = [];
 
 var objLoader = null, jsonLoader = null;
 
-var duration = 300; // ms
 var currentTime = Date.now();
+var fowardPosition = null;
+
+
+function loadPaths(order, obj, png){
+  if(!objLoader)
+      objLoader = new THREE.OBJLoader();
+
+  objLoader.load(
+      obj,
+
+      function(object)
+      {
+          var texture = new THREE.TextureLoader().load(png);
+          // var normalMap = new THREE.TextureLoader().load('../models/cerberus/Cerberus_N.jpg');
+          // var specularMap = new THREE.TextureLoader().load('../models/cerberus/Cerberus_M.jpg');
+
+          object.traverse( function ( child )
+          {
+              if ( child instanceof THREE.Mesh )
+              {
+                  child.castShadow = true;
+                  child.receiveShadow = true;
+                  child.material.map = texture;
+              }
+          } );
+          object.scale.set(3,3,4);
+          object.position.z = (order * 4) - 2;
+          object.position.x = 0;
+          object.position.y = -4;
+          // player.rotation.x = Math.PI / 180 * 15;
+          // player.rotation.y = -3;
+          scene.add(object);
+
+          // return object;
+
+      },
+      function ( xhr ) {
+
+          console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+      },
+      // called when loading has errors
+      function ( error ) {
+
+          console.log( 'An error happened' );
+
+      });
+
+    playerBBox = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+}
 
 function loadObj(obj, png){
   if(!objLoader)
@@ -48,12 +109,13 @@ function loadObj(obj, png){
 
           player = object;
           player.scale.set(3,3,3);
-          player.position.z = 0;
+          player.position.z = 2;
           player.position.x = 0;
-          player.position.y = -4;
+          player.position.y = -2.8;
           // player.rotation.x = Math.PI / 180 * 15;
           // player.rotation.y = -3;
-          console.log(player);
+          // console.log(player);
+          player.add(camera);
           scene.add(player);
 
           // return object;
@@ -94,13 +156,16 @@ function loadCar(obj, png){
               }
           } );
 
+          var rand = Math.floor(Math.random()*roadsPosition.length);
+
           car = object;
-          car.scale.set(5,5,5);
+          car.scale.set(4,4,4);
           car.rotation.y = -Math.PI/2;
-          car.position.z = 9;
-          car.position.x = 25;
-          car.position.y = -4;
-          scene.add(car);
+          car.position.z = (roadsPosition[rand] * 4) - 2;
+          car.position.x = 35;
+          car.position.y = -3.3;
+          car.name = carBBox.length - 1;
+          cars.add(car);
 
           // return object;
 
@@ -117,7 +182,58 @@ function loadCar(obj, png){
 
       });
 
-      carBBox = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+      carBBox.push(new THREE.Box3(new THREE.Vector3(), new THREE.Vector3()));
+
+
+}
+function loadLogs(obj, png){
+  if(!objLoader)
+      objLoader = new THREE.OBJLoader();
+
+  objLoader.load(
+      obj,
+
+      function(object)
+      {
+          var texture = new THREE.TextureLoader().load(png);
+
+          object.traverse( function ( child )
+          {
+              if ( child instanceof THREE.Mesh )
+              {
+                  child.castShadow = true;
+                  child.receiveShadow = true;
+                  child.material.map = texture;
+              }
+          } );
+
+          var rand = Math.floor(Math.random()*riversPosition.length);
+
+          log = object;
+          log.scale.set(4,4,4);
+          // log.rotation.y = -Math.PI/2;
+          log.position.z = (riversPosition[rand] * 4) - 2;
+          log.position.x = 35;
+          log.position.y = -5;
+          log.name = logBBox.length - 1;
+          logs.add(log);
+
+          // return object;
+
+      },
+      function ( xhr ) {
+
+          console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+      },
+      // called when loading has errors
+      function ( error ) {
+
+          console.log( 'An error happened' );
+
+      });
+
+      logBBox.push(new THREE.Box3(new THREE.Vector3(), new THREE.Vector3()));
 
 
 }
@@ -125,7 +241,6 @@ function loadTree(obj, png){
   if(!objLoader)
       objLoader = new THREE.OBJLoader();
 
-  treeBBox = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
 
   objLoader.load(
       obj,
@@ -147,15 +262,23 @@ function loadTree(obj, png){
           } );
 
           // player = object;
+          var x;
+          do {
+            x = random(-36,36);
+          } while (x % 4 != 0);
+
+          var rand = Math.floor(Math.random()*grassPosition.length);
+
           object.scale.set(4,4,4);
-          object.position.z = 5;
-          object.position.x = 3;
-          object.position.y = -4;
+          object.position.z = (grassPosition[rand]*4)-2;
+          object.position.x = x;
+          object.position.y = -3;
           // player.rotation.x = Math.PI / 180 * 15;
           // player.rotation.y = -3;
-          treeBBox.setFromObject(object);
-
-          scene.add(object);
+          tmp = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+          tmp.setFromObject(object);
+          treeBBox.push(tmp);
+          trees.add(object);
 
           // return object;
 
@@ -177,16 +300,40 @@ function loadTree(obj, png){
 
 function animate() {
 
-    var now = Date.now();
-    var deltat = now - currentTime;
-    currentTime = now;
-    var fract = deltat / duration;
-    var velocity = Math.PI * 2 * fract;
+  var duration1 = 100;
+  var duration2 = 150;
+  var duration3 = 200;
+  var duration4 = 250;
+  var now = Date.now();
+  var deltat = now - currentTime;
+  currentTime = now;
+  var fract1 = deltat / duration1;
+  var fract2 = deltat / duration2;
+  var fract3 = deltat / duration3;
+  var fract4 = deltat / duration4;
+  var velocity1 = Math.PI * 2 * fract1;
+  var velocity2 = Math.PI * 2 * fract2;
+  var velocity3 = Math.PI * 2 * fract3;
+  var velocity4 = Math.PI * 2 * fract4;
 
-    car.position.x -= velocity;
+  for (var i in cars.children) {
+    var rand = random(1,10);
+    if(rand < 5)
+      cars.children[i].position.x -= velocity3;
+    else
+      cars.children[i].position.x -= velocity4;
 
-    if(car.position.x < -25 )
-      car.position.x = 25;
+
+    if(cars.children[i].position.x < -35 )
+      cars.children[i].position.x = 35;
+  }
+
+  for (var i in logs.children) {
+    logs.children[i].position.x -= velocity;
+
+    if(logs.children[i].position.x < -35 )
+      logs.children[i].position.x = 35;
+  }
 }
 
 function run() {
@@ -202,30 +349,45 @@ function run() {
   // Update the camera controller
   orbitControls.update();
 
-  playerBBox.setFromObject(player)
-  carBBox.setFromObject(car)
-
-  if(carBBox.containsPoint(player.position)){
-    player.animation.stop();
-    player.position.set(0,-4,0);
+  if (pathsPositions[fowardPosition] != undefined && player.animation.running == false) {
+    if (pathsPositions[fowardPosition] == 'road') {
+      player.position.y = -3.3;
+    } else if (pathsPositions[fowardPosition] == 'grass') {
+      player.position.y = -2.8;
+    } else if (pathsPositions[fowardPosition] == 'river') {
+      player.position.y = -3.05;
+    }
   }
-  // playerBBox.setFromObject(player)
+
+  playerBBox.setFromObject(player)
+
+  for (var element in carBBox) {
+    carBBox[element].setFromObject(cars.children[element])
+    if(carBBox[element].containsPoint(player.position)){
+      player.animation.stop();
+      player.position.set(0,-3.3,2);
+    }
+  }
 }
 
 function onKeyDown(event){
   // console.log(event.keyCode);
   switch (event.keyCode) {
     case 65:
-      // IZQ
-      if(treeBBox.containsPoint(new THREE.Vector3(player.position.x+1, player.position.y, player.position.z)))
+      var crash = false;
+      for (var i in treeBBox) {
+        // IZQ
+        if(treeBBox[i].containsPoint(new THREE.Vector3(player.position.x+1, player.position.y, player.position.z)))
+          {crash = true; break;} else
+        if(treeBBox[i].containsPoint(new THREE.Vector3(player.position.x+2, player.position.y, player.position.z)))
+          {crash = true; break;} else
+        if(treeBBox[i].containsPoint(new THREE.Vector3(player.position.x+3, player.position.y, player.position.z)))
+          {crash = true; break;} else
+        if(treeBBox[i].containsPoint(new THREE.Vector3(player.position.x+4, player.position.y, player.position.z)))
+          {crash = true; break;}
+      }
+      if (crash == true)
         break;
-      if(treeBBox.containsPoint(new THREE.Vector3(player.position.x+2, player.position.y, player.position.z)))
-        break;
-      if(treeBBox.containsPoint(new THREE.Vector3(player.position.x+3, player.position.y, player.position.z)))
-        break;
-      if(treeBBox.containsPoint(new THREE.Vector3(player.position.x+4, player.position.y, player.position.z)))
-        break;
-
       // console.log(new THREE.Vector3(player.position.x+4, player.position.y, player.position.z));
       // console.log(treeBBox.containsPoint(new THREE.Vector3(player.position.x, player.position.y, player.position.z)));
       animations(new THREE.Vector3(1,0,0));
@@ -233,43 +395,63 @@ function onKeyDown(event){
     break;
 
     case 87:
-      // ARRIBA
-      if(treeBBox.containsPoint(new THREE.Vector3(player.position.x, player.position.y, player.position.z+1)))
+      for (var i in treeBBox) {
+        // ARRIBA
+        if(treeBBox[i].containsPoint(new THREE.Vector3(player.position.x, player.position.y, player.position.z+1)))
+          {crash = true; break;} else
+        if(treeBBox[i].containsPoint(new THREE.Vector3(player.position.x, player.position.y, player.position.z+2)))
+          {crash = true; break;} else
+        if(treeBBox[i].containsPoint(new THREE.Vector3(player.position.x, player.position.y, player.position.z+3)))
+          {crash = true; break;} else
+        if(treeBBox[i].containsPoint(new THREE.Vector3(player.position.x, player.position.y, player.position.z+4)))
+          {crash = true; break;}
+      }
+
+      if (crash == true)
         break;
-      if(treeBBox.containsPoint(new THREE.Vector3(player.position.x, player.position.y, player.position.z+2)))
-        break;
-      if(treeBBox.containsPoint(new THREE.Vector3(player.position.x, player.position.y, player.position.z+3)))
-        break;
-      if(treeBBox.containsPoint(new THREE.Vector3(player.position.x, player.position.y, player.position.z+4)))
-        break;
+
       animations(new THREE.Vector3(0,0,1));
       player.animation.start();
+      fowardPosition++;
     break;
 
     case 68:
-      // DER
-      if(treeBBox.containsPoint(new THREE.Vector3(player.position.x-1, player.position.y, player.position.z)))
+      for (var i in treeBBox) {
+        // DER
+        if(treeBBox[i].containsPoint(new THREE.Vector3(player.position.x-1, player.position.y, player.position.z)))
+          {crash = true; break;} else
+        if(treeBBox[i].containsPoint(new THREE.Vector3(player.position.x-2, player.position.y, player.position.z)))
+          {crash = true; break;} else
+        if(treeBBox[i].containsPoint(new THREE.Vector3(player.position.x-3, player.position.y, player.position.z)))
+          {crash = true; break;} else
+        if(treeBBox[i].containsPoint(new THREE.Vector3(player.position.x-4, player.position.y, player.position.z)))
+          {crash = true; break;}
+      }
+
+      if (crash == true)
         break;
-      if(treeBBox.containsPoint(new THREE.Vector3(player.position.x-2, player.position.y, player.position.z)))
-        break;
-      if(treeBBox.containsPoint(new THREE.Vector3(player.position.x-3, player.position.y, player.position.z)))
-        break;
-      if(treeBBox.containsPoint(new THREE.Vector3(player.position.x-4, player.position.y, player.position.z)))
-        break;
+
       animations(new THREE.Vector3(-1,0,0));
       player.animation.start();
+      fowardPosition--;
     break;
 
     case 83:
-      // ABAJO
-      if(treeBBox.containsPoint(new THREE.Vector3(player.position.x, player.position.y, player.position.z-1)))
+      for (var i in treeBBox) {
+        // ABAJO
+        if(treeBBox[i].containsPoint(new THREE.Vector3(player.position.x, player.position.y, player.position.z-1)))
+          {crash = true; break;} else
+        if(treeBBox[i].containsPoint(new THREE.Vector3(player.position.x, player.position.y, player.position.z-2)))
+          {crash = true; break;} else
+        if(treeBBox[i].containsPoint(new THREE.Vector3(player.position.x, player.position.y, player.position.z-3)))
+          {crash = true; break;} else
+        if(treeBBox[i].containsPoint(new THREE.Vector3(player.position.x, player.position.y, player.position.z-4)))
+          {crash = true; break;}
+      }
+
+      if (crash == true)
         break;
-      if(treeBBox.containsPoint(new THREE.Vector3(player.position.x, player.position.y, player.position.z-2)))
-        break;
-      if(treeBBox.containsPoint(new THREE.Vector3(player.position.x, player.position.y, player.position.z-3)))
-        break;
-      if(treeBBox.containsPoint(new THREE.Vector3(player.position.x, player.position.y, player.position.z-4)))
-        break;
+
       animations(new THREE.Vector3(0,0,-1));
       player.animation.start();
     break;
@@ -278,7 +460,7 @@ function onKeyDown(event){
   }
 
   animated = true;
-  console.log(playerBBox);
+  // console.log(playerBBox);
 
 }
 
@@ -294,10 +476,10 @@ async function animations(vector){
                 {
                     keys:[0, .25, .5, 1],
                     values:[
-                            { x: player.position.x + (vector.x * 1), y : -4, z: player.position.z + (vector.z * 1) },
-                            { x: player.position.x + (vector.x * 2) , y : -3, z: player.position.z + (vector.z * 2) },
-                            { x: player.position.x + (vector.x * 3) , y : -3, z: player.position.z + (vector.z * 3) },
-                            { x: player.position.x + (vector.x * 4) , y : -4, z: player.position.z + (vector.z * 4) },
+                            { x: player.position.x + (vector.x * 1), y : player.position.y, z: player.position.z + (vector.z * 1) },
+                            { x: player.position.x + (vector.x * 2) , y : -2, z: player.position.z + (vector.z * 2) },
+                            { x: player.position.x + (vector.x * 3) , y : -2, z: player.position.z + (vector.z * 3) },
+                            { x: player.position.x + (vector.x * 4) , y : player.position.y, z: player.position.z + (vector.z * 4) },
                             ],
                     target:player.position
                 },
@@ -335,44 +517,66 @@ function createScene(canvas) {
     camera = new THREE.PerspectiveCamera( 45, canvas.width / canvas.height, 1, 4000 );
     camera.position.set(-10, 10, -10);
     // camera.rotation.set(30, 30, 30);
+    // player = new THREE.Object3D;
     scene.add(camera);
 
     // Create a group to hold all the objects
     root = new THREE.Object3D;
 
-
-
     ambientLight = new THREE.AmbientLight ( 0xffffff );
     root.add(ambientLight);
 
+    trees = new THREE.Object3D;
+    cars = new THREE.Object3D;
+    logs = new THREE.Object3D;
+
+    scene.add(trees);
+    scene.add(cars);
+    scene.add(logs);
+
+    fowardPosition = 1;
+
+    var rand;
+
+    for (var i = 0; i < 50; i++) {
+      var obj = [
+        './assets/models/environment/road/0/0.obj', './assets/models/environment/road/1/0.obj',
+        './assets/models/environment/grass/0/0.obj', './assets/models/environment/grass/1/0.obj',
+        './assets/models/environment/river/0.obj'
+      ];
+      var png = [
+        './assets/models/environment/road/0/0.png', './assets/models/environment/road/1/0.png',
+        './assets/models/environment/grass/0/0.png', './assets/models/environment/grass/1/0.png',
+        './assets/models/environment/river/0.png'
+      ];
+      rand = Math.floor(Math.random()*(4-0+1)+0);
+      if (rand == 0 || rand == 1) {
+        roadsPosition.push(i);
+        pathsPositions.push('road');
+      } else if (rand == 2 || rand == 3) {
+        grassPosition.push(i);
+        pathsPositions.push('grass');
+      } else if (rand == 4) {
+        riversPosition.push(i);
+        pathsPositions.push('river');
+      }
+      loadPaths(i, obj[rand], png[rand]);
+    }
+
+    rand = random(10, 100);
     // Create the objects
     loadObj('./assets/models/characters/chicken/0.obj', './assets/models/characters/chicken/0.png');
-    loadTree('./assets/models/environment/tree/3/0.obj', './assets/models/environment/tree/3/0.png');
-    loadCar('./assets/models/vehicles/blue_car/0.obj', './assets/models/vehicles/blue_car/0.png');
+    for (var i = 0; i < rand; i++) {
+      let type = random(0,3);
+      loadTree('./assets/models/environment/tree/'+type+'/0.obj', './assets/models/environment/tree/'+type+'/0.png');
+      loadCar('./assets/models/vehicles/blue_car/0.obj', './assets/models/vehicles/blue_car/0.png');
+      loadLogs('./assets/models/environment/log/0/0.obj', './assets/models/environment/log/0/0.png');
+    }
     // loadObj('./assets/models/characters/chicken/0.obj');
 
     // Create a group to hold the objects
     group = new THREE.Object3D;
     root.add(group);
-
-    // Create a texture map
-    var map = new THREE.TextureLoader().load(mapUrl);
-    map.wrapS = map.wrapT = THREE.RepeatWrapping;
-    map.repeat.set(8, 8);
-
-    var color = 0xffffff;
-
-    // Put in a ground plane to show off the lighting
-    geometry = new THREE.PlaneGeometry(200, 200, 50, 50);
-    var mesh = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({color:color, map:map, side:THREE.DoubleSide}));
-
-    mesh.rotation.x = -Math.PI / 2;
-    mesh.position.y = -4.02;
-
-    // Add the mesh to our group
-    group.add( mesh );
-    mesh.castShadow = false;
-    mesh.receiveShadow = true;
 
     // Now add the group to our scene
     scene.add( root );
@@ -380,4 +584,8 @@ function createScene(canvas) {
     orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
 
     animations(new THREE.Vector3(0,0,0));
+}
+
+function random(min, max){
+  return Math.floor(Math.random()*(max-min+1)+min);
 }
